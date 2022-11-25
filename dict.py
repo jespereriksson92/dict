a3a5fff9a3b2fee11fcb0ece17e262a280920c82
jespereriksson92 <erjesper@gmail.com>
2022-11-25 14:47:05 +0100

    replaced conn with connection(already replaced C)

--- dict.py
+++ dict.py
@@ -1,43 +1,43 @@
 import psycopg2
 
 def get_db_connection():
-    conn = psycopg2.connect(
+    connection = psycopg2.connect(
             user="postgres",
             password="5731",
             host="localhost",
             port="5432",
             database="dict")
-    return conn
+    return connection
 
 def read_dict():
-    conn = get_db_connection()
-    cur = conn.cursor()
+    connection = get_db_connection()
+    cur = connection.cursor()
     cur.execute("SELECT id, word, translation FROM dictionary;")
     rows = cur.fetchall()
     cur.close()
-    conn.close()
+    connection.close()
     return rows
 
 def add_word(word, translation):
-    conn = get_db_connection()
-    cur = conn.cursor()
+    connection = get_db_connection()
+    cur = connection.cursor()
     cur.execute(f"INSERT INTO dictionary (word, translation) VALUES ('{word}', '{translation}');")
     cur.close()
-    conn.close()
+    connection.close()
 
 def delete_word(ID):
-    conn = get_db_connection()
-    cur = conn.cursor()
+    connection = get_db_connection()
+    cur = connection.cursor()
     cur.execute(f"DELETE FROM dictionary WHERE id = '{ID}';")
     cur.close()
-    conn.close()
+    connection.close()
 
 def save_dict():
-    conn = get_db_connection()
-    cur = conn.cursor()
+    connection = get_db_connection()
+    cur = connection.cursor()
     cur.execute("COMMIT;")
     cur.close()
-    conn.close()
+    connection.close()
 
 def insert_word(word, translation):
     print (f"To insert {word} and its translation {translation}, use add instead!")
